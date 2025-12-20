699170897d584ca5be9d22e5684d5f9470d38a10
TheGrayCuber <graycuber@gmail.com>
2025-12-19 22:05:14 -0800

    update controls with D from url

--- quadratic/setup.js
+++ quadratic/setup.js
@@ -89,6 +89,12 @@ function setup() {
 	}
 	textBox.Items.push(new arrowItem(1,ring_list,bigText,'ring'));
 	textBox.Items.push(new textItem(0.8,'i² = -1',bigText*0.8,'ring_rule'));
+	for (let r = 0; r < D_list.length; r++){
+		if (D_list[r] == D){
+			textBox.Items[textBox.Items.length-2].Index = r;
+			break;
+		}
+	}
 	
 	textBox.Items.push(new textItem(1,'',0));
 	textBox.Items.push(new textItem(0.6,'color mode',bigText*0.6));
