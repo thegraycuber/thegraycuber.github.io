61892a9202d606b8bb2b6c4ec388adaf89d909b8
TheGrayCuber <graycuber@gmail.com>
2026-08-03 11:45:22 -0700

    crab typo

--- crabman/crab.js
+++ crabman/crab.js
@@ -19,7 +19,6 @@ function createPaletteDisplays(paletteList){
 		}
 
 		outerString += '</div>';
-	--color-half: #288856;
 			
 		document.getElementById('palette-holder').appendChild(paletteDisplay);
 		paletteDisplay.outerHTML = outerString;
