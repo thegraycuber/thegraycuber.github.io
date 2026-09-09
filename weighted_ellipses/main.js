3dd27a00aba259b068e9d7816c0943899459f2d1
TheGrayCuber <graycuber@gmail.com>
2026-09-09 15:41:59 -0700

    fifth time's the charm?

--- weighted_ellipses/main.js
+++ weighted_ellipses/main.js
@@ -65,8 +65,8 @@ function draw(){
 		ellipseShader.setUniform("origin", [origin.x,height-origin.y]);
 		ellipseShader.setUniform("scalar", scalar);
 	} else {
-		ellipseShader.setUniform("origin", [origin.x*2,(2*defaultOrigin.y-origin.y)*2]);
-		ellipseShader.setUniform("scalar", scalar/2);
+		ellipseShader.setUniform("origin", [origin.x/2,(2*defaultOrigin.y-origin.y)/2]);
+		ellipseShader.setUniform("scalar", scalar*2);
 	}
 
 	let distance = getTost(border,points,weights);
