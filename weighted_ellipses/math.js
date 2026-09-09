b22fe048dcaaa3935cceb10d00f41b1a6c633c2b
TheGrayCuber <graycuber@gmail.com>
2026-09-09 14:53:27 -0700

    test glsl fix

--- weighted_ellipses/math.js
+++ weighted_ellipses/math.js
@@ -72,7 +72,7 @@ attribute vec2 aTexCoord;
 
 void main() {
   vec4 positionVec4 = vec4(aPosition, 1.0);
-//   positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+  positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
   gl_Position = positionVec4;
 }
 `;
