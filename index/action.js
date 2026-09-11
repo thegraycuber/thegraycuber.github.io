af327fae91fa802c75fb68641bf4da20f23f4655
TheGrayCuber <graycuber@gmail.com>
2026-09-11 16:00:31 -0700

    negative ellipse video!

--- index/action.js
+++ index/action.js
@@ -155,6 +155,7 @@ function randomPalette(){
 
 
 var videoLinks = [
+	'https://youtu.be/S9-S7knul30', // negative ellipses
 	'https://youtu.be/VKeg2NtzJAo', // ellipses with 3 foci
 	'https://youtu.be/SbYee_gWLv0', // 3d bases
 	'https://youtu.be/d6agN416onM', // imaginary bases
